71cdc681d9e43e44653e3210b1a7aa4120a87bd2
Michael <michael@jbiblio.de>
2013-12-29 22:31:09 +0100

    Create controller that returns some dummy services

--- serstapa/ui/src/main/webapp/js/main.js
+++ serstapa/ui/src/main/webapp/js/main.js
@@ -1,1 +1,14 @@
 var app = angular.module('serstapa', []);
+
+/**
+ * Controller to manage the services.
+ */
+app.controller("ServicesController", [ "$scope", function($scope) {
+	$scope.services = [ {
+		"name" : "Service A",
+		"status" : "running"
+	}, {
+		"name" : "Service B",
+		"status" : "offline"
+	} ];
+} ]);
